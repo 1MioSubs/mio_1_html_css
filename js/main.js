717b588ecbc8427c36c849502dbbbad900090bc9
1MioSubs <it09022022@gmail.com>
2023-07-23 16:45:51 +0300

    slider fin 90%

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-// alert('Hello World')
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+    
