42cbfc2f0f7f3a61ea163ea8cd250c9059e0029e
1MioSubs <it09022022@gmail.com>
2023-07-24 14:17:22 +0300

    adaptive fin

--- js/main.js
+++ js/main.js
@@ -24,6 +24,15 @@ function open(evt) {
     .classList.add("tabs__content-item--active");
 }
 
+const menuBtn = document.querySelector(".menu__btn");
+const menu = document.querySelector(".menu__list");
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active');
+})
+
+
+
 var swiper = new Swiper(".mySwiper", {
   effect: "fade",
   pagination: {
